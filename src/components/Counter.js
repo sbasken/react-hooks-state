a5d8a89ff8099e9b315f436933f4350b643d8504
Saki <sbasken@gmail.com>
2023-02-21 10:17:47 -0800

    changed onclick to inline function

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -3,11 +3,11 @@ import React, { useState } from "react";
 function Counter() {
   const [count, setCount] = useState(0);
 
-  function increment() {
-    setCount(count + 1);
-  }
+  // function increment() {
+  //   setCount(count + 1);
+  // }
 
-  return <button onClick={increment}>I have been clicked {count} times</button>;
+  return <button onClick={()=>setCount(count + 1)}>I have been clicked {count} times</button>;
 }
 
 export default Counter;
